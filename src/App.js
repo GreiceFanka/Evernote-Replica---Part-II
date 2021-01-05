1aac30d074967d1c7ff1dd4a263467208bb4b464
Greice <greicefcardoso@gmail.com>
2021-01-05 18:48:44 -0300

    Created components

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, {Fragment} from 'react';
+import './App.scss';
+import {Notification,Section} from 'rbx';
+import Routes from '../src/routes';
+
+const App = () =>(
+  <Fragment>
+    <Routes/>
+  </Fragment>
+)
+ 
+
+export default App;
