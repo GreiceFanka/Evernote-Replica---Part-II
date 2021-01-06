add571c328b5e7b6548628f712247179d2e676f5
Greice <greicefcardoso@gmail.com>
2021-01-06 20:16:00 -0300

    Setting path

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
-import Home from './screens/home';
+import HomeScreen from './screens/home';
+import UserEdit from './screens/users/edit/index';
+import LoginScreen from './screens/auth/login/index';
+import RegisterScreen from './screens/auth/register';
+import NotesScreen from './screens/notes/index';
 
 const Routes = () => (
   <BrowserRouter>
     <Switch>
-      <Route exact path='/' component={Home}  />
+      <Route exact path='/' component={HomeScreen}  />
+      <Route exact path='/register' component={RegisterScreen} />
+      <Route exact path='/login' component={LoginScreen} />
+      <Route exact path='/notes' component={NotesScreen} />
+      <Route exact path='/users/edit' component={UserEdit} />
     </Switch>
   </BrowserRouter>
 )
