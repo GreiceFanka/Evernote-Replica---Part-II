e5cc2d07714d2861eda7d8a5fa95b842c35105a2
Greice <greicefcardoso@gmail.com>
2021-01-12 19:12:52 -0300

    Added list and notes

--- src/components/notes/index.js
+++ src/components/notes/index.js
@@ -2,8 +2,36 @@ import React, { Fragment, useEffect, useState } from 'react';
 import { push as Menu } from 'react-burger-menu';
 import { Column, Button } from 'rbx';
 import '../../styles/notes.scss';
+import List from '../notes/list';
+import NoteService from '../../services/note';
 
-function Notes (props){
+function Notes(props) {
+  const [notes, setNotes] = useState([]);
+  const [current_note, setCurrentNote] = useState({ title: "", body: "", id: "" });
+
+  async function fetchNotes() {
+    const response = await NoteService.index();
+    if (response.data.length >= 1) {
+      setNotes(response.data.reverse())
+      setCurrentNote(response.data[0])
+    }
+  }
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+  const createNote = async (params) => {
+    const note = await NoteService.create();
+    fetchNotes();
+  }
+
+  const selectNote = (id) => {
+    const note = notes.find((note) => {
+      return note._id == id;
+    })
+    setCurrentNote(note);
+  }
   return (
     <Fragment>
       <Column.Group className="notes" id="notes">
@@ -21,7 +49,11 @@ function Notes (props){
               Search...
             </Column>
           </Column.Group>
-          <p>List...</p>
+          <List
+            notes={notes}
+            selectNote={selectNote}
+            current_note={current_note}
+            createNote={createNote} />
         </Menu>
 
 
